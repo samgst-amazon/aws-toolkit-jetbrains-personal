b29aa27b15e2b10f740cac09bfbad4ec741e9a1f
samgst-amazon <samgst@amazon.com>
2025-01-14 11:10:41 -0800

    local build cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -60,7 +60,7 @@ if (regionEnv.isPresent && bucketEnv.isPresent && prefixEnv.isPresent) {
     // TODO: can we serve a remote cache out of CloudFront instead? https://docs.gradle.org/8.1/userguide/build_cache.html#sec:build_cache_configure_remote
     buildCache {
         local {
-            isEnabled = false
+            isEnabled = true
         }
 
         remote<com.github.burrunan.s3cache.AwsS3BuildCache> {
